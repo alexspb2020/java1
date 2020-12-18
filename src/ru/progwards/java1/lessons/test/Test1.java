f3411b89f9b603f3254f2b6873b03f16b63aad4f
alexspb <abkozyrev@mail.ru>
2020-12-18 20:08:00 +0300

    Start2

--- src/ru/progwards/java1/lessons/test/Test1.java
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -1,7 +1,6 @@
-package u.progwards.java1.lessons.test;
-
+package ru.progwards.java1.lessons.test;
 public class Test1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
     }
 }
